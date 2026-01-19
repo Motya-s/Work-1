6254644e275975fe986d882e989597a6fa597a4d
Bonsai <matpro.ru2@gmail.com>
2026-01-19 16:08:53 +0300

    Add files via upload

---
+++ Зд 2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class ProgramConfig
+
+{
+    
+    public const string Version = "v1.5.0";
+    public const string DeveloperName = "Иван 2-ой курс";
+
+    
+    public static void ShowConfig()
+
+    {
+
+        Console.WriteLine("Информация о программе");
+        Console.WriteLine($"Версия: {Version}");
+        Console.WriteLine($"Разработчик: {DeveloperName}");
+
+    }
+
+}
+
+
+class TestConfig
+
+{
+
+    static void Main()
+
+    {
+
+        ProgramConfig.ShowConfig();
+
+        
+        Console.WriteLine($"Текущая версия: {ProgramConfig.Version}");
+
+        Console.ReadLine();
+
+    }
+
+}
